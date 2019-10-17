c9d9593d70d69e8a4272ace3a466a18558cf1e3c
VoidHobo <alanhe.wifi@gmail.com>
2019-10-17 01:27:04 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $( "document" ).ready(function() {
+    let vow;
+    let vow = ["a", "e", "i", "o", "u"];
+    $("#button").click(function(){
+        console.log("test")
+    
+    
+    
+    
+    $("#result").append();
 
 
-
+    });
 });
